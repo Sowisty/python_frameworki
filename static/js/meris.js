f4e8f9af3b5aea1cda9a8a6dac2c8ac4641cdfb4
Sowisty <mateusz.malopolski@gmail.com>
2025-03-27 12:17:30 +0100

    Poprawka merisia

--- static/js/meris.js
+++ static/js/meris.js
@@ -25,7 +25,7 @@ var canvas = document.querySelector("#unity-canvas");
         updateBannerVisibility();
       }
 
-      var buildUrl = "Build";
+      var buildUrl = "http://127.0.0.1:8000/static/Build";
       var loaderUrl = buildUrl + "/VR_CLASS_WEB.loader.js";
       var config = {
         arguments: [],
